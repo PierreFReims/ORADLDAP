332daa0e09d0ab05341d41b29022f6b64d968e9f
Pierre Fontaine <pierre.fontaine2@viacesi.fr>
2023-12-12 12:40:26 +0100

    check acls and check user password expiration

--- crawler.py
+++ crawler.py
@@ -1,54 +1,75 @@
 import configparser
-from ldap3 import Server, Connection, SAFE_SYNC
+from ldap3 import Server, Connection, SAFE_SYNC, SUBTREE, BASE
 
 class Crawler:
-    dn = None
-    password = None
+    rootdn_user = None
+    rootdn_password = None
     uri = None
     base_dn = None
+    ldap_server = None
+    suffix = None
 
     def __init__(self, config_path=None):
         if config_path:
             config = configparser.ConfigParser()
             config.read(config_path)
 
-            self.dn = config.get('auth', 'dn')
-            self.password = config.get('auth', 'password')
+            self.rootdn_user = config.get('auth', 'rootdn_user')
+            self.rootdn_password = config.get('auth', 'rootdn_password')
             self.uri = config.get('auth', 'uri')
             self.base_dn = config.get('auth', 'base_dn')
+            self.suffix = config.get('auth', 'base_dn')
+            self.ldap_server = Server(self.uri, get_info=all)
 
-            # Initialize an LDAP Server object
-            self.ldap_server = Server(self.uri)
             print("[Authenticated mode]")
         else:
             print("[Anonymous mode]")
 
         print("Crawler initialized...")
             
-
-    def check_ppolicy()->bool:
-
-        # LDAP server details
-        ldap_server = Server('ldap://your-ldap-server:389', get_info=ALL)
-
-        # Establish an LDAP connection
-        with Connection(ldap_server, user=ldap_dn, password=ldap_password, auto_bind=True) as conn:
-            # Search for password policy-related attributes
-            base_dn = 'cn=subschema'
-            filter_str = '(objectClass=*)'
-            attributes = ['subschemaSubentry']
-
-            conn.search(search_base=base_dn, search_filter=filter_str, search_scope=SUBTREE, attributes=attributes)
-
-            # Check if password policy attributes are present
-            entry = LDAPEntry(conn, base_dn)
-            if 'subschemaSubentry' in entry:
-                subschema_subentry = entry['subschemaSubentry'].value
-                if 'pwdPolicySubentry' in conn.server.schema.get_entry(subschema_subentry):
-                    return True
-            else:
-                print("Unable to determine password policy configuration.")
-        return False
+    def check_ppolicy(self) -> bool:
+        try:
+            with Connection(self.uri, self.rootdn_user, self.rootdn_password, auto_bind=True) as conn:
+                conn.search(search_base='', search_filter='(objectClass=*)', search_scope=BASE)
+                supported_controls = conn.result.get('controls', [])
+                if any(control['controlType'] == '1.3.6.1.4.1.42.2.27.8.5.1' for control in supported_controls):
+                    print("[Password Policy] - Enabled")
+                else:
+                    print("[Password Policy] - Disabled or not supported")
+        except Exception as e:
+            print(f"Error checking password policy: {e}")
         
-    def check_acl()->bool:
+    def check_acl(self) -> bool:
+        try:
+            with Connection(self.uri, self.rootdn_user, self.rootdn_password, auto_bind=True) as conn:
+                conn.search(search_base='cn=config', search_filter='(olcDatabase={1}mdb)', search_scope=SUBTREE, attributes=['olcAccess'])
+                attrs = str(conn.entries[0]).split('\n')
+                if conn.entries:
+                    print("[ACLs] - Enabled")
+                else:
+                    print("[ACLs] - Disabled or not supported")
+
+        except Exception as e:
+            print(f"Error checking ACLs: {e}")
         return False
+
+    def check_privileged_users_permissions(self) -> bool:
+        attributes_check = ["pwdExpireWarning", "pwdMaxAge"]
+        try:
+            with Connection(self.uri, self.rootdn_user, self.rootdn_password, auto_bind=True) as conn:
+                conn.search(search_base=self.suffix, search_filter='(|(objectClass=person)(objectClass=inetOrgPerson)(objectClass=organizationalPerson))',attributes=attributes_check)
+            if conn.entries:
+                print("[User Password Expiration Date] - Enabled")
+            else:
+                print("[User Password Expiration Date] - Disabled or not supported")
+        except Exception as e:
+            print(f"Error checking attribute: {e}")
+            return False
+
+    def check_asleep_accounts(self) -> bool:
+        return False
+
+    def Run(self):
+      self.check_ppolicy()  
+      self.check_acl()
+      self.check_privileged_users_permissions()
--- main.py
+++ main.py
@@ -2,33 +2,10 @@
 
 import sys
 from crawler import *
-from ldap3 import Server, Connection, SAFE_SYNC
-
-def ldap_query(config_file=None):
-    config = configparser.ConfigParser()
-    config.read(config_file)
-
-    ldap_dn = config.get('auth', 'dn')
-    ldap_password = config.get('auth', 'password')
-    ldap_uri = config.get('auth', 'uri')
-    ldap_base_dn = config.get('auth', 'base_dn')
-
-    ldap_server = Server(ldap_uri, get_info=all)
-    
-    with Connection(ldap_server, user=ldap_dn, password=ldap_password, auto_bind=True) as conn:
-        base_dn = ldap_base_dn
-        filter_str = '(objectClass=*)'
-        attributes = ['cn', 'sn', 'mail']
-
-        conn.search(search_base=base_dn, search_filter=filter_str, attributes=attributes)
-        for entry in conn.entries:
-            print(entry.entry_dn)
-            #for attr in attributes:
-            #    print(f"{attr}: {entry[attr]}")
-            print('-' * 30)
 
 if __name__ == "__main__":
     if (len(sys.argv) != 1):
-        Crawler(sys.argv[1])
+        crawler = Crawler(sys.argv[1])
     else:
-        Crawler()
+        crawler = Crawler()
+    crawler.Run()
